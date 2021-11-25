15cf96e05af22e70fbd69ba9ce60a580fc734fe8
sh3ngsh3ng <yshenglim13@gmail.com>
2021-11-25 06:26:23 +0000

    added migration file to add fk to cart_items

---
+++ migrations/20211125062242-add-fk-to-cart-items.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('cart_items', 'product_slots_id', {
+    type: 'int',
+    unsigned: true,
+    notNull: true,
+    foreignKey: {
+      name: 'cart_items_product_slots_id',
+      table: "product_slots",
+      rules: {
+        onDelete: 'cascade',
+        onUpdate: 'restrict'
+      },
+      mapping: 'id'
+    }
+  })
+};
+
+exports.down = function(db) {
+  return db.removeForeignKey('cart_items, product_slots_id').then(()=>{
+    db.removeColumn('cart_items', 'product_slots_id')
+  })
+};
+
+exports._meta = {
+  "version": 1
+};
