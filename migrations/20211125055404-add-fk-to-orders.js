8752639c0d53ef0cf91fbcc895d37e5766d88810
sh3ngsh3ng <yshenglim13@gmail.com>
2021-11-25 06:02:09 +0000

    added users and orders migrations

---
+++ migrations/20211125055404-add-fk-to-orders.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('orders', 'user_id', {
+    type: 'int',
+    unsigned: true,
+    notNull: true,
+    foreignKey: {
+      name: 'order_user_fk',
+      table: 'users',
+      rules: {
+        onDelete: 'cascade',
+        onUpdate: 'restrict'
+      },
+      mapping: 'id'
+    }
+  })
+};
+
+exports.down = function(db) {
+  return db.removeForeignKey('orders', 'order_user_fk').then(()=>{
+    return db.removeColumn('orders', "user_id")
+  })
+};
+
+exports._meta = {
+  "version": 1
+};
