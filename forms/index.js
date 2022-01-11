75e661f5cf0ead93ab998de4df19f86e020025d1
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-11 12:14:26 +0000

    added utils.js for forms, moved datetimelocal function to utils.js, moved roomTypes to utils.js

--- forms/index.js
+++ forms/index.js
@@ -4,73 +4,7 @@ const forms = require("forms");
 const fields = forms.fields;
 const validators = forms.validators;
 const widgets = forms.widgets
-const tag  = require("../node_modules/forms/lib/tag.js")
-const reduce = require('../node_modules/reduce')
-
-var dataRegExp = /^data-[a-z]+([-][a-z]+)*$/;
-var ariaRegExp = /^aria-[a-z]+$/;
-var legalAttrs = [
-    'autocomplete', 'autocorrect', 'autofocus', 'autosuggest', 'checked', 'dirname', 'disabled', 'tabindex', 'list', 'max', 'maxlength', 'min', 'novalidate', 'pattern', 'placeholder', 'readonly', 'required', 'size', 'step'
-];
-var ignoreAttrs = [
-    'id', 'name', 'class', 'classes', 'type', 'value', 'multiple'
-];
-var getUserAttrs = function (opt) {
-    return reduce(opt, function (attributes, option, k) {
-        if ((ignoreAttrs.indexOf(k) === -1 && legalAttrs.indexOf(k) > -1) || dataRegExp.test(k) || ariaRegExp.test(k)) {
-            // eslint-disable-next-line no-param-reassign
-            attributes[k] = option;
-        }
-        return attributes;
-    }, {});
-};
-
-var input = function (type) {
-    return function (opts) {
-        var opt = opts || {};
-        var userAttrs = getUserAttrs(opt);
-        var w = {
-            classes: opt.classes,
-            type: type,
-            formatValue: function (value) {
-                return value || value === 0 ? value : null;
-            }
-        };
-        w.toHTML = function (name, field) {
-            var f = field || {};
-            var attrs = {
-                type: type,
-                name: name,
-                id: f.id === false ? false : f.id || true,
-                classes: w.classes,
-                value: w.formatValue(f.value)
-            };
-            return tag('input', [
-                attrs, userAttrs, w.attrs || {}
-            ]);
-        };
-        return w;
-    };
-};
-var datetimeLocalWidget = input('datetime-local');
-let datetimeLocal = function (options) {
-    var opt = options || {};
-    var w = datetimeLocalWidget(opt);
-    w.formatValue = function (value) {
-        if (!value) {
-            return null;
-        }
-
-        var date = is.date(value) ? value : new Date(value);
-
-        if (isNaN(date.getTime())) {
-            return null;
-        }
-
-        return date.toISOString().slice(0, 23);
-    };
-    return w;
-};
+const {datetimeLocal, roomTypes} = require("./utils")
 
 
 
@@ -145,6 +79,15 @@ const createProductForm = (tags) => {
             },
             widget: widgets.multipleSelect(),
             choices: tags
+        }),
+        'room_type': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            widget: widgets.select(),
+            choices: roomTypes
         })
     })
 }
