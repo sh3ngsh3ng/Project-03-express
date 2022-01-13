5190299662fa8c77ed6d934aac8f995dd4e9a4d4
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-13 17:14:22 +0000

    added manage-orders page and route

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,14 @@ const router = express.Router()
 const ordersDataLayer = require("../dal/orders")
 
 
-// display all orders of a product slot
+// display all orders of a vendor
+router.get("/all", async (req,res) => {
+    console.log("All Orders")
+    res.send("All Orders")
+})
+
+
+// display all orders of a specific product slot
 router.get("/:productSlotId", async (req, res) => {
     let orders = await ordersDataLayer.getSpecificOrderItems(req.params.productSlotId)
     res.render('products/manage-orders', {
