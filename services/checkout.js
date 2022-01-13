55d13a5ceb985440f59d730e616684760a145f97
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-13 10:04:09 +0000

    cleaned code for debug

--- services/checkout.js
+++ services/checkout.js
@@ -12,17 +12,12 @@ const onCheckOut = async(orders, userId, totalCost) => {
 
     // update quantity slots
     await checkoutDataLayer.editProductSlotsQuantityOnCheckOut(orders)
-    console.log("1 pass")
     // update user's cart
     await cartDataLayer.deleteCart(userId)
-    console.log("2 pass")
     // create user's order
     let newOrder = await ordersDataLayer.createOrder(userId, totalCost) // newOrder is the new row created in orders table
-    console.log("newOrder =>", newOrder)
-    console.log("3 pass")
     // create order items
     await ordersDataLayer.createOrderItems(orders, newOrder)
-    console.log("4 pass") 
     return
 }
 
