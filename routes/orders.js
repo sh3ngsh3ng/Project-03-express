568a1c1adf799817ea8404c60281fcf127e111ea
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-16 10:02:22 +0000

    added userid fk to orderitems table

--- routes/orders.js
+++ routes/orders.js
@@ -2,13 +2,11 @@ const express = require("express")
 const router = express.Router()
 const ordersDataLayer = require("../dal/orders")
 const { createFilterForm, bootstrapField } = require("../forms")
-const { ProductSlot } = require("../models")
+const { Product, ProductSlot, OrderItem } = require("../models")
 
 
 // display all orders of a vendor
 router.get("/", async (req,res) => {
-    
-    console.log("result => ", result.toJSON())
     const filterForm = createFilterForm().toHTML(bootstrapField)
     let orders = await ordersDataLayer.getOrderItems(req.session.vendor.id)
     res.render('products/manage-orders', {
@@ -16,6 +14,79 @@ router.get("/", async (req,res) => {
     })
 })
 
+router.post("/", async (req,res) => {
+    console.log("called")
+    console.log("body =>", req.body)
+    let filterForm = createFilterForm()
+
+    filterForm.handle(req, {
+        'empty': async(form) => {
+            // display all
+            let orders = await ordersDataLayer.getOrderItems(req.session.vendor.id)
+            let filterForm = form.toHTML(bootstrapField)
+            res.render('products/manage-orders', {
+                orders, filterForm
+            })
+        },
+        'error': async(form) => {
+            // display all
+            let orders = await ordersDataLayer.getOrderItems(req.session.vendor.id)
+            let filterForm = form.toHTML(bootstrapField)
+            req.flash("error_messages", "No results found")
+            res.render('products/manage-orders', {
+                orders, filterForm
+            })
+        },
+        'success': async(form) => {
+            // return search resutls
+
+            let payment_status = req.body.payment_status
+            let name = req.body.name
+            let q = OrderItem.where({'vendor_id': req.session.vendor.id})
+            filterForm = filterForm.toHTML(bootstrapField)
+
+            if (payment_status) {
+                if (payment_status == "processing") {
+                    q = q.where({"order_item_status": "processing"})
+                }
+
+                if (payment_status == "paid") {
+                    q = q.where({"order_item_status": "paid"})
+                }
+
+                if (payment_status == "cancelled") {
+                    q = q.where({"order_item_status": "cancelled"})
+                }
+
+            }
+
+            
+            // if (name) {
+            //     try {
+            //     q =  q.query('join', 'order_items', 'orders.id', 'order_id')
+            //     console.log(q)
+            //     let result = await q.fetchAll()
+            //     result = result.toJSON()
+            //     console.log("result =>", result)
+            //     res.send("Success")}
+            //     catch (e) {
+            //         console.log("error =>", e)
+            //     }
+            // }
+
+            let orders = await q.fetchAll({
+                require: false,
+                withRelated: ['order', 'order.user']
+            })
+            console.log(orders)
+            orders = orders.toJSON()
+            res.render('products/manage-orders', {
+                orders, filterForm
+            })
+        }
+    })
+    
+})
 
 // change to post to "/"
 // display all orders of a specific product slot
