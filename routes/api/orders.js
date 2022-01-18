2d8e0690d61a55d33a9bf0cfdae2690b5d8a3dbf
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-18 19:35:18 +0000

    s

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -24,20 +24,26 @@ router.get("/order-history", async(req,res)=>{
     // let orderHistory = await OrderItem.where({
     //     "user_id": req.query.userId,
     //     "order_item_status": "confirmed"
-    // }).fetchAll({
+    // }).orderBy('order_date', 'DESC').fetchAll({
     //     require: false,
     //     withRelated: ['order', 'productslot', 'productslot.product']
     // })
     // res.json(orderHistory)
     let orderHistory = await Order.where({
         "user_id": req.query.userId
-    }).fetchAll({
+    }).orderBy('order_date', 'DESC').fetchAll({
         require: false,
         withRelated: ['orderitems', 'orderitems.productslot', 'orderitems.productslot.product']
     })
     res.json(orderHistory)
 })
 
+// sorting
+// let result = await ProductSlot.collection().orderBy('slot_datetime', 'DESC').fetch({
+    //     require: false
+    // }) // ASC
+
+// add orders
 
 // router.get("/notifications")
 
